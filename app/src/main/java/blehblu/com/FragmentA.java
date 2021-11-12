d49bb4393f1d1ff6f6b9a8ee8c089ecce58467e6
Aryan Ritesh <aryanrtsh@gmail.com>
2021-11-12 18:22:06 +0530

    Transfer completed the sending of data from fragment to activity is working

--- app/src/main/java/blehblu/com/FragmentA.java
+++ app/src/main/java/blehblu/com/FragmentA.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class FragmentA extends Fragment {
-
+    EditText name1,age1;
+    Button done;
     public FragmentA() {
         // Required empty public constructor
     }
@@ -18,7 +21,22 @@ public class FragmentA extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View view=inflater.inflate(R.layout.fragment_a,container,false);
+        name1 =view.findViewById(R.id.TextPersonName);
+        age1=view.findViewById(R.id.age);
+        done=view.findViewById(R.id.done);
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName=name1.getText().toString();
+                int userAge=Integer.valueOf(age1.getText().toString());
+                fragmentToA FA= (fragmentToA) getActivity();
+                assert FA != null;
+                FA.getData(userName,userAge);
+
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/blehblu/com/fragmentToA.java
+++ app/src/main/java/blehblu/com/fragmentToA.java
@@ -13,13 +13,19 @@ public class fragmentToA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_to);
-        name=findViewById(R.id.personName);
-        age=findViewById(R.id.personAge);
 
         FragmentManager fragmentManager= getSupportFragmentManager();
         FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
         FragmentA fragmentToActivity = new FragmentA();
         fragmentTransaction.add(R.id.display,fragmentToActivity); // two parameters, 1. layout's name 2. object of the fragment
         fragmentTransaction.commit();
+    }
+
+    public void getData(String userName,int userAge){
+        name=findViewById(R.id.personName);
+        age=findViewById(R.id.personAge);
+        name.setText("Hello "+userName);
+        age.setText("Your age is "+userAge);
+
     }
 }
